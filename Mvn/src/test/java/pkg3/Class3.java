5bc0824dd392d003232d9b63c16cd565520a89d6
hemanth5718 <hemanthgowda571@gmail.com>
2025-07-29 12:27:57 +0530

    e

--- Mvn/src/test/java/pkg3/Class3.java
+++ Mvn/src/test/java/pkg3/Class3.java
@@ -6,6 +6,17 @@ public class Class3 {
 	@Test
 	public void jen() {
 		System.out.println("help");
+		
+		String browser = System.getProperty("browser");
+		String url = System.getProperty("url");
+		String username = System.getProperty("username");
+		String password = System.getProperty("password");
+		
+		System.out.println(browser);
+		System.out.println(url);
+		System.out.println(username);
+		System.out.println(password);
+		
 	}
 
 }
