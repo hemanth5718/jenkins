b4f1308acfb988f4a6b0f7da1919dc0fbfc2560c
hemanth5718 <hemanthgowda571@gmail.com>
2025-07-29 10:00:33 +0530

    hi

---
+++ Mvn/src/test/java/pkg1/mvn1_Test.java
@@ -0,0 +1,16 @@
+package pkg1;
+
+import org.testng.annotations.Test;
+
+public class mvn1_Test {
+	@Test
+	public void createOrgTest() {
+		System.out.println("createOrg");
+	}
+	
+	@Test
+	public void deleteOrgTest() {
+		System.out.println("deleteOrg");
+	}
+
+}
---
+++ Mvn/src/test/java/pkg2/Mvn2_Test.java
@@ -0,0 +1,16 @@
+package pkg2;
+
+import org.testng.annotations.Test;
+
+public class Mvn2_Test {
+	@Test
+	public void createcontact_Test() {
+		System.out.println("createcontact");
+	}
+	
+	@Test
+	public void deleteContact_Test() {
+		System.out.println("deleteContact");
+	}
+
+}
